c18d9f871fdd6392b4bfe5e8c5fbb213524c4b0d
Julien Rouhaud <julien.rouhaud@free.fr>
2016-10-23 12:19:57 +0200

    Force read-only activity in the past

--- pg_timelord.c
+++ pg_timelord.c
@@ -26,13 +26,14 @@
 #include "utils/snapshot.h"
 #include "utils/timestamp.h"
 #include "utils/tqual.h"
+#include "tcop/utility.h"
 
 
 PG_MODULE_MAGIC;
 
 
-/* saved hook address in case of unload */
 static ExecutorStart_hook_type prev_ExecutorStart = NULL;
+static ProcessUtility_hook_type prev_ProcessUtility = NULL;
 
 /*--- Functions --- */
 
@@ -42,11 +43,20 @@ bool	check_pgtl_ts(char **newval, void **extra, GucSource source);
 void	assign_pgtl_ts(const char *newval, void *extra);
 
 static void pgtl_ExecutorStart(QueryDesc *queryDesc, int eflags);
+static void pgtl_ProcessUtility(Node *parsetree,
+		const char *queryString, ProcessUtilityContext context,
+		ParamListInfo params,
+		DestReceiver *dest, char *completionTag);
+
+void pgtl_preventWrite(void);
+void pgtl_restoreWrite(void);
 bool HeapTupleSatisfiesTimeLord(HeapTuple htup, Snapshot snapshot,
 								Buffer buffer);
 
 static char *pgtl_ts_char;
 static TimestampTz pgtl_ts = 0;
+static bool old_XactReadOnly;
+static bool old_XactReadOnly_changed = false;
 
 void
 _PG_init(void)
@@ -65,6 +75,8 @@ _PG_init(void)
 	/* Install hook */
 	prev_ExecutorStart = ExecutorStart_hook;
 	ExecutorStart_hook = pgtl_ExecutorStart;
+	prev_ProcessUtility = ProcessUtility_hook;
+	ProcessUtility_hook = pgtl_ProcessUtility;
 
 	DefineCustomStringVariable("pg_timelord.ts",
 							 "Timestamp with time zone destination",
@@ -83,6 +95,7 @@ _PG_fini(void)
 {
 	/* uninstall hook */
 	ExecutorStart_hook = prev_ExecutorStart;
+	ProcessUtility_hook = prev_ProcessUtility;
 }
 
 bool
@@ -125,6 +138,8 @@ assign_pgtl_ts(const char *newval, void *extra)
 static void
 pgtl_ExecutorStart (QueryDesc *queryDesc, int eflags)
 {
+	pgtl_preventWrite();
+
 	/* do not mess with non MVCC snapshots, like index build :) */
 	if (pgtl_ts != 0 && IsMVCCSnapshot(queryDesc->snapshot))
 		queryDesc->snapshot->satisfies = HeapTupleSatisfiesTimeLord;
@@ -133,6 +148,99 @@ pgtl_ExecutorStart (QueryDesc *queryDesc, int eflags)
 		prev_ExecutorStart(queryDesc, eflags);
 	else
 		standard_ExecutorStart(queryDesc, eflags);
+
+	pgtl_restoreWrite();
+}
+
+static void
+pgtl_ProcessUtility(Node *parsetree,
+		const char *queryString, ProcessUtilityContext context,
+		ParamListInfo params,
+		DestReceiver *dest, char *completionTag)
+{
+	pgtl_preventWrite();
+
+	/* forbid most of utility command in the past! */
+	if (pgtl_ts != 0)
+	{
+		switch (nodeTag(parsetree))
+		{
+			case T_TransactionStmt:
+				{
+					TransactionStmt *stmt = (TransactionStmt *) parsetree;
+
+					/* error out if explicitely asked rw transaction */
+					if (stmt->kind == TRANS_STMT_BEGIN ||
+						stmt->kind == TRANS_STMT_START)
+					{
+						ListCell *lc;
+
+						foreach(lc, stmt->options)
+						{
+							DefElem    *item = (DefElem *) lfirst(lc);
+
+							if (strcmp(item->defname, "transaction_read_only") == 0)
+							{
+								A_Const *con = (A_Const *) item->arg;
+
+								Assert(IsA(item->arg, A_Const) && IsA(&con->val, Integer));
+								if (!intVal(&con->val))
+								{
+									elog(ERROR, "You cannot ask for read-write transaction in the past!");
+								}
+							}
+						}
+					}
+				}
+			case T_VariableSetStmt:
+			case T_VariableShowStmt:
+			case T_ExplainStmt:
+			case T_NotifyStmt:
+			case T_ListenStmt:
+			case T_UnlistenStmt:
+			case T_CopyStmt:
+			case T_PrepareStmt:
+			case T_DeallocateStmt:
+			case T_PlannedStmt:
+			case T_ClosePortalStmt:
+			case T_FetchStmt:
+			case T_DoStmt:
+				/* whitelist of utility command */
+				break;
+			default:
+				elog(ERROR, "You cannot run utility statement in the past!");
+		}
+	}
+
+	if (prev_ProcessUtility)
+	(*prev_ProcessUtility) (parsetree, queryString,
+								context, params,
+								dest, completionTag);
+	else
+		standard_ProcessUtility(parsetree, queryString,
+								context, params,
+								dest, completionTag);
+
+	pgtl_restoreWrite();
+}
+
+void pgtl_preventWrite(void)
+{
+	if (pgtl_ts != 0)
+	{
+		old_XactReadOnly = XactReadOnly;
+		XactReadOnly = true;
+		old_XactReadOnly_changed = true;
+	}
+}
+
+void pgtl_restoreWrite(void)
+{
+	if (old_XactReadOnly_changed)
+	{
+		XactReadOnly = old_XactReadOnly;
+		old_XactReadOnly_changed = false;
+	}
 }
 
 bool
